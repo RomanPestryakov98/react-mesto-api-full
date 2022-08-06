9ea9d7b818a8a57c6881704370d50b52bfb075f9
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-08-06 21:32:02 +0300

    added CORS methods

--- backend/express-mesto-gha/app.js
+++ backend/express-mesto-gha/app.js
@@ -6,16 +6,29 @@ const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const allowedCors = require('./cors/allowedCors');
 
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use((req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+
   next();
 });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
