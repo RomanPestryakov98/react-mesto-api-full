e9e2d7b0a1c00ca16cfff10bb2a2ab7e5c6d8be0
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-08-07 13:53:56 +0300

    fix cors

--- backend/express-mesto-gha/app.js
+++ backend/express-mesto-gha/app.js
@@ -11,19 +11,15 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  const { origin } = req.headers;
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
-});
-
-app.use((req, res, next) => {
-  const { origin } = req.headers;
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
