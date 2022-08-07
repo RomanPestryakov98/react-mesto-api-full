306f8efa6aa76158e6fe5448f66c81a6296dca0a
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-08-07 14:07:39 +0300

    fix cors

--- backend/express-mesto-gha/app.js
+++ backend/express-mesto-gha/app.js
@@ -12,6 +12,7 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use((req, res, next) => {
+  console.log(allowedCors)
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
@@ -22,7 +23,7 @@ app.use((req, res, next) => {
   }
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
   }
 
   next();
